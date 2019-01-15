501264b8087eda4ffde9530e6eef29363b4ac5d1
papusBocoum <3036521131@qq.com>
2019-01-15 15:52:33 +0800

    all

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,39 +1,33 @@
-//Selected DOM Items
-
+// Select DOM Items
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
 
-//Set the initial state of Menu
-
+// Set Initial State Of Menu
 let showMenu = false;
 
-menuBtn.addEventListener("click", toogleMenu);
+menuBtn.addEventListener("click", toggleMenu);
 
-function toogleMenu() {
+function toggleMenu() {
   if (!showMenu) {
     menuBtn.classList.add("close");
     menu.classList.add("show");
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
-
     navItems.forEach(item => item.classList.add("show"));
 
-    //Set the Menu state
-
+    // Set Menu State
     showMenu = true;
   } else {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
-
     navItems.forEach(item => item.classList.remove("show"));
 
-    //Set the Menu state
-
+    // Set Menu State
     showMenu = false;
   }
 }
